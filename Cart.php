461ba79599cb279fcff40d376963982fe7b15d80
001 <001@mail>
2022-11-25 17:39:26 +0700

    fix cart

---
+++ Cart.php
@@ -0,0 +1,26 @@
+<!-- Button trigger modal -->
+<a type="button" class="icon me-5" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+<span class="material-symbols-outlined">
+<i>shopping_cart</i>
+</span>
+     </a>
+     
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
