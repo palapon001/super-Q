1fb280ca49de50533df3f54771b7039b65db680e
TIME <34773021+palapon001@users.noreply.github.com>
2022-12-01 13:06:10 +0000

    fix

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
  <?php include 'condb.php'; ?>
  <form name="form1" method="post" action="check_login.php">
-    <h1>Login</h1>
+    <h1>Login System</h1>
     <p>
       <input name="username" type="text" id="username" placeholder="Username"  class="form-control">
     </p>
