4e816f40d6509e6044430739400ee5b76e9209c7
TIME <34773021+palapon001@users.noreply.github.com>
2022-12-03 10:50:23 +0000

    fix

--- adduser/adduser.php
+++ adduser/adduser.php
@@ -22,17 +22,17 @@ mysqli_query($con, "SET NAMES UTF8");
         <div class="card-body">
             <form neme="form" method="post" action="adduser-add.php">
                 <label>เพิ่ม user : </label>
-                <input type="text" name="user_id" placeholder="user_id" />
-                <select name="hname">
+                <input class="mt-3 form-control" type="text" name="user_id" placeholder="user_id" />
+                <select class="mt-3 form-control" name="hname">
                     <option value="" selected disabled hidden></option>
                     <option value="นาย">นาย</option>
                     <option value="นางสาว">นางสาว</option>
                     <option value="นาง">นาง</option>
                 </select>
-                <input type="text" name="name" placeholder="name" />
-                <input type="text" name="lname" placeholder="lname">
-                <input type="number" name="tel" placeholder="tel">
-                <input type="submit" neme="save" value="save" />
+                <input  class="mt-3 form-control" type="text" name="name" placeholder="name" />
+                <input class="mt-3 form-control" type="text" name="lname" placeholder="lname">
+                <input class="mt-3 form-control" type="number" name="tel" placeholder="tel">
+                <input class="mt-3 form-control" type="submit" neme="save" value="save" />
             </form>
             <p>
             <p>
