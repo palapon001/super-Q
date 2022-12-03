0b837d31b3567d1363c1153f4636f29be0ed950e
TIME <34773021+palapon001@users.noreply.github.com>
2022-12-03 10:37:44 +0000

    fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'bootstrap.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<body>
+<body style="background-color:MediumSeaGreen">
     <div class="h-100 d-flex align-items-center justify-content-center">
         <div class="card" style="width: 25rem;">
             <div class="card-body">
