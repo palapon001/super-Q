49082a7d7afc864138e0027b92337dfd19e5b8c6
TIME <34773021+palapon001@users.noreply.github.com>
2022-12-26 09:35:40 +0000

    fix

--- Cart-showtable.php
+++ Cart-showtable.php
@@ -34,7 +34,7 @@
                 var input1 = parseInt($('#the_input_id<?php echo $no; ?>').val());
                 var totals = parseFloat(input1 * <?php echo $f['TotalPrice']; ?>) || 0;
                 $('#total<?php echo $no; ?>').text(totals);
-                $('totalsum').text(totals);
+                $('#totalsum').text(totals);
               };
 
             });
