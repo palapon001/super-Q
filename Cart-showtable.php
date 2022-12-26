cd702107477218965e99bce8dad2a1d40593bfcc
TIME <34773021+palapon001@users.noreply.github.com>
2022-12-26 10:19:58 +0000

    fix

--- Cart-showtable.php
+++ Cart-showtable.php
@@ -42,9 +42,9 @@
           </script>
 
 <input class="form-control" type="text" id="total<?php echo $no; ?>" name="number" value="<?php echo $f['TotalPrice']; ?>"/>
-          <!-- <div name="pree" id="total<?php echo $no; ?>" value="">
+          <div name="pree" id="total<?php echo $no; ?>" value="">
             
-          </div> -->
+          </div>
 
 
         </td>
