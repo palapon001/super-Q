94796e5cf2bad4b95b8b8a83430b2ffb992f894b
Palapon Thitithanaporn <palapon.thit@kkpfg.com>
2022-12-28 15:50:14 +0700

    fix

--- Cart-showtable.php
+++ Cart-showtable.php
@@ -2,50 +2,55 @@
 
 
 <div class="table-responsive">
- 
-    <table class="table">
+
+  <table class="table">
+    <tr>
+      <td>ลำดับ</td>
+      <td>ชื่อสินค้า</td>
+      <td>จำนวน</td>
+      <td>ราคา</td>
+      <td>ลบ</td>
+    </tr>
+    <?php
+    $no = 1;
+    $sql = " SELECT * FROM cart ORDER BY Cartno ASC ";
+    $q = mysqli_query($con, $sql);
+    while ($f = mysqli_fetch_assoc($q)) {
+    ?>
       <tr>
-        <td>ลำดับ</td>
-        <td>ชื่อสินค้า</td>
-        <td>จำนวน</td>
-        <td>ราคา</td>
-        <td>ลบ</td>
-      </tr>
-      <?php
-      $no = 1;
-      $sql = " SELECT * FROM cart ORDER BY Cartno ASC ";
-      $q = mysqli_query($con, $sql);
-      while ($f = mysqli_fetch_assoc($q)) {
-      ?>
-        <tr>
-          <td><?php echo $no; ?></td>
-          <td><?php echo $f['ItemName']; ?> </td>
-          <td>
-            <input type="text" name="ip" id="ip<?php echo $no; ?>" class="form-control" value="" required>
-          </td>
-          <td>
-            <div class="input-group mb-3 w-75">
+        <td><?php echo $no; ?></td>
+        <td><?php echo $f['ItemName']; ?> </td>
+        <td>
+          <input type="text" name="ip<?php echo $no; ?>" id="ip<?php echo $no; ?>" class="form-control" value="" required>
+        </td>
+        <td>
+          <div class="input-group mb-3 w-75">
             <form id="lines-form-1">
-              <input class="form-control" id="i<?php echo $no; ?>" name="total" type="text" value="0">
+              <input class="form-control" id="i<?php echo $no; ?>" name="total<?php echo $no; ?>" type="text" value="0" disabled>
             </form>
-            </div>
+          </div>
 
 
-          </td>
-          <td><a href='Cart-del.php?Cartno=<?php echo $f['Cartno']; ?>' class="btn btn-danger" onclick="return confirm('ต้องการจะลบหรือไม่')">ลบ</a></td>
+        </td>
+        <td><a href='Cart-del.php?Cartno=<?php echo $f['Cartno']; ?>' class="btn btn-danger" onclick="return confirm('ต้องการจะลบหรือไม่')">ลบ</a></td>
 
-        </tr>
+      </tr>
+      <script>
+        $('input[name=ip<?php echo $no; ?>]').on('keyup', function() {
+          var qty = this.value; ;
+            $('input[name=total<?php echo $no; ?>]').val(qty*<?php echo $f['TotalPrice']; ?>);
+        });
+      </script>
+    <?php
+      $no++;
+    }
 
-      <?php
-        $no++;
-      }
-
-      echo "</table>";
-      ?>
-  <h1>
-    ราคารวม :
-    <div id="tot-qty">0</div>
-  </h1>
+    echo "</table>";
+    ?>
+    <h1>
+      ราคารวม :
+      <div id="tot-qty">0</div>
+    </h1>
 
 </div>
 
