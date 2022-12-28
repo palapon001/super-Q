49057cc50a3071947fae74c0946d3c81ac3c0168
Palapon Thitithanaporn <palapon.thit@kkpfg.com>
2022-12-28 15:02:07 +0700

    fix

--- Cart-showtable.php
+++ Cart-showtable.php
@@ -1,85 +1,60 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-          <script>
-          $(document).ready(function(){
-            updatesum();
-          });
-          
-          function updatesum(){
-            sum=0;
-            $("input[name^='total']").each(function(){
-              sum+=Number($(this).val());
-            });
-            $("#sumofaudi").val(sum);
-          }
-          </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
 
 <div class="table-responsive">
-  <table class="table">
-    <tr>
-      <td>ลำดับ</td>
-      <td>ชื่อสินค้า</td>
-      <td>จำนวน</td>
-      <td>ราคา</td>
-      <td>ลบ</td>
-    </tr>
-    <?php
-    $no = 1;
-    $totalsum = 0;
-    $sql = " SELECT * FROM cart ORDER BY Cartno ASC ";
-    $q = mysqli_query($con, $sql);
-    while ($f = mysqli_fetch_assoc($q)) {
-    ?>
+ 
+    <table class="table">
       <tr>
-        <td><?php echo $no; ?></td>
-        <td><?php echo $f['ItemName']; ?> </td>
-        <td>
-          <input type="text" id="the_input_id<?php echo $no; ?>" class="form-control" value="" onchange="updatesum();"  required>
-        </td>
-        <td>
-          <!-- <script type='text/javascript' src='//code.jquery.com/jquery-1.11.0.js'></script> -->
-
-          <!-- <script type='text/javascript'>
-            $(function() {
-
-              $('#the_input_id<?php echo $no; ?>').keyup(function() {
-                updateTotal();
-              });
-
-              var updateTotal = function() {
-                var input1 = parseInt($('#the_input_id<?php echo $no; ?>').val());
-                var totals = parseFloat(input1 * <?php echo $f['TotalPrice']; ?>) || 0;
-                $('#total<?php echo $no; ?>').text(totals);
-              };
-
-            });
-          </script> -->
-         
-
-            <input name="total" type="text" value="3" onchange="updatesum();">
-
-          <!-- <div name="pree" id="total<?php echo $no; ?>" value="">
-            <?php //echo $f['TotalPrice']; ?>
-          </div> -->
-
-
-        </td>
-        <td><a href='Cart-del.php?Cartno=<?php echo $f['Cartno']; ?>' class="btn btn-danger" onclick="return confirm('ต้องการจะลบหรือไม่')">ลบ</a></td>
-
+        <td>ลำดับ</td>
+        <td>ชื่อสินค้า</td>
+        <td>จำนวน</td>
+        <td>ราคา</td>
+        <td>ลบ</td>
       </tr>
+      <?php
+      $no = 1;
+      $sql = " SELECT * FROM cart ORDER BY Cartno ASC ";
+      $q = mysqli_query($con, $sql);
+      while ($f = mysqli_fetch_assoc($q)) {
+      ?>
+        <tr>
+          <td><?php echo $no; ?></td>
+          <td><?php echo $f['ItemName']; ?> </td>
+          <td>
+            <input type="text" name="ip" id="ip<?php echo $no; ?>" class="form-control" value="" required>
+          </td>
+          <td>
+            <div class="input-group mb-3 w-75">
+            <form id="lines-form-1">
+              <input class="form-control" id="i<?php echo $no; ?>" name="total" type="text" value="0">
+            </form>
+            </div>
 
-    <?php
-      $totalsum += $f['TotalPrice'];
-      $no++;
-    }
 
-    echo "</table>";
+          </td>
+          <td><a href='Cart-del.php?Cartno=<?php echo $f['Cartno']; ?>' class="btn btn-danger" onclick="return confirm('ต้องการจะลบหรือไม่')">ลบ</a></td>
 
-    ?>
-    <h1>
-      ราคารวม :
-      <div id="totalsum">
+        </tr>
 
-      </div>
-    </h1>
+      <?php
+        $no++;
+      }
+
+      echo "</table>";
+      ?>
+  <h1>
+    ราคารวม :
+    <div id="tot-qty">0</div>
+  </h1>
 
 </div>
+
+<script>
+  $('form#lines-form-1 :input').change(function() {
+    var tot = 0;
+    $("form#lines-form-1 :input").each(function() {
+      tot += Number($(this).val());
+    });
+    $('#tot-qty').text(tot);
+  });
+</script>
