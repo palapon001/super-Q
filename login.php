4efea9d9a3e94b24e0167cf67b013710488eed62
Palapon Thitithanaporn <palapon.thit@kkpfg.com>
2022-12-28 17:28:10 +0700

    fix

--- login.php
+++ login.php
@@ -1,14 +1,7 @@
  <?php include 'condb.php'; ?>
  <form name="form1" method="post" action="check_login.php">
-    <h1>Login System</h1>
-    <p>
-      <input name="username" type="text" id="username" placeholder="Username"  class="form-control">
-    </p>
-    <p>
-      <input name="password" type="password" id="password" placeholder="Password"  class="form-control">
-    </p>
-    <p>
-      <input type="submit" name="Submit" value="Login"  class="btn btn-primary">
-    </p>
-    
-  </form>
+   <h1>Login System</h1>
+   <p><input name="username" type="text" id="username" placeholder="ชื่อผู้ใช้" class="form-control"></p>
+   <p><input name="password" type="password" id="password" placeholder="รหัสผ่าน" class="form-control"></p>
+   <p><input type="submit" name="Submit" value="เข้าสู่ระบบ" class="btn btn-primary"></p>
+ </form>
